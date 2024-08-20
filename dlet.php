f53daea0703d31bb8fb38ef04c70832e8adbda21
AnouarAndichi <anwar.dreno@gmail.com>
2024-08-20 23:14:45 +0200

    Update game.phtml and fix resources

--- dlet.php
+++ dlet.php
@@ -1,4 +1,5 @@
-<?php 
+ <?php 
+ /*
 include("t5rg/config.php"); 
 $link = mysql_connect($AppConfig['db']['host'],$AppConfig['db']['user'],$AppConfig['db']['password']) or die(mysql_error()); 
 mysql_select_db($AppConfig['db']['database'],$link) or die(mysql_error()); 
