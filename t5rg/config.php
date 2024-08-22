42351622ec690fa824954d0b71d99df8cf22a19a
AnouarAndichi <anwar.dreno@gmail.com>
2024-08-22 21:20:26 +0200

    update farm

--- t5rg/config.php
+++ t5rg/config.php
@@ -6,17 +6,17 @@ ini_set('display_startup_errors', 1);
 
 $AppConfig = array (
         'db'                                         => array (
-/* 
+
                 'host'                                => 'php56-db',
                 'user'                                => 'Anwar123',
                 'password'                        => 'Anwar123',
                 'database'                        => 'Anwar123',
-               */
+               /* 
                 'host'                                => 'localhost',
                 'user'                                => 'root',
                 'password'                        => '',
                 'database'                        => 'tatar',
-                
+                */
         ),
                 'Game'                         => array (
                 'speed'                      => '10000', // سرعة اللعبه
