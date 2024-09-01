2bee8c3d4dd5bee38f08dafd2fd14acaca91d1f8
AnouarAndichi <anwar.dreno@gmail.com>
2024-09-02 00:07:47 +0200

    add index

--- index.php
+++ index.php
@@ -1,7 +1,210 @@
-<?php
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>حرب التتار - TATARWAR SMARTSERVS V2.00</title>
+    <link rel="stylesheet" type="text/css" href="index_files/main.css" />
+    <meta name="content-language" content="en" />
+    <meta http-equiv="imagetoolbar" content="no" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <script src="index_files/js.js" type="text/javascript"></script>
+    <script src="index_files/news.js" type="text/javascript"></script>
+    <script src="index_files/news2.js" type="text/javascript"></script>
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon" />
+</head>
+
+<body class="presto indexPage">
+    <div class="wrapper">
+        <div id="header">
+            <h1>مرحبا بك في حرب التتار</h1>
+        </div>
+
+        <div id="navigation">
+            <a href="index.php" class="home"><img src="index_files/x.gif" alt="tatar-smartservs" /></a>
+
+            <table class="menu">
+                <tr>
+                    <td><a class="signup_link mark" href="?signup"><span><b>التسجيل</b></span></a></td>
+
+                    <td><a class="login_link" href="?login"><span><b>تسجيل الدخول</b></span></a></td>
+                </tr>
+            </table>
+        </div>
+
+        <div id="register_now">
+            <a class="signup_link" href="?signup">التسجيل</a><span>العب الآن مجانا</span>
+        </div>
+
+        <div id="content">
+            <div class="grit">
+                <div class="infobox">
+                    <div id="what_is_travian">
+                        <h2>ما هى حرب التتار ؟</h2>
+
+                        <p>حرب التتار هى <b>لعبة تصفح مجانية</b> لا تحتاج الى تحميل ،وهي عبارة عن لعبة حرب في عالم مليء باللاعبين الحقيقين .</p>
+
+                        <p class="play_now" style="margin-right: 10px;margin-top: 10px;"><a class="signup_link" href="?signup"><img src="index_files/register.png" alt="" /></a></p>
+                    </div>
+
+                    <div id="player_counter">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <th>عدد اللاعبين:</th>
+
+                                    <td><? echo $players_count1 + $players_count2 + $players_count3 + $players_count4 + $players_count5; ?></td>
+                                </tr>
+
+                                <tr>
+                                    <th>اللاعبون النشطون:</th>
+
+                                    <td><? echo $players_count1 + $players_count2 + $players_count3 + $players_count4 + $players_count5; ?></td>
+                                </tr>
+
+                                <tr>
+                                    <th>الموجودون حالياً:</th>
+
+                                    <td><? echo $online_players_count1 + $online_players_count2 + $online_players_count3 + $online_players_count4 + $online_players_count5; ?></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div id="about_the_game">
+                        <h2>عن لعبة حرب التتار :</h2>
+
+                        <ul>
+                            <li>سوف تبدأ كرئيس لقرية صغيرة.</li>
+
+                            <li>يمكنك بناء القرى او شن الحروب ،او التجارة مع الجيران.</li>
+
+                            <li>سوف تلعب مع او ضد الكثير من اللاعبين.</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="secondarybox">
+                    <div id="screenshots">
+                        <h2>صور من اللعبة</h2>
+                        <div id="screenshots_preview">
+                            <ul id="screenshot_list">
+                                <li><a href=""><img src="index_files/s1s00000.jpg" alt="Screenshot" /></a></li>
+
+                                <li><a href=""><img src="index_files/s2s00000.jpg" alt="Screenshot" /></a></li>
+
+                                <li><a href=""><img src="index_files/s4s00000.jpg" alt="Screenshot" /></a></li>
+
+                                <li><a href=""><img src="index_files/s3s00000.jpg" alt="Screenshot" /></a></li>
+
+                                <li><a href=""><img src="index_files/s5s00000.jpg" alt="Screenshot" /></a></li>
+
+                                <li><a href=""><img src="index_files/s7s00000.jpg" alt="Screenshot" /></a></li>
+
+                                <li><a href=""><img src="index_files/s8s00000.jpg" alt="Screenshot" /></a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div id="newsbox">
+                      <h2>الاخبار</h2><div class="news"><p class="date">الاصدار الجديد من سمارت سيرفس SS V2.00</p><p align="right"><span id="typing"> <b> امتلك سيرفرات حرب التتار من سمارت سيرفس بأفضل الأسعار تواصل معنا عبر الواتساب 00201149338526
+
+</span></p><script type="text/javascript">function _dfkjEXe(){document.write(decodeURIComponent(escape(window.atob("PC9zcGFuPiAgCjxzY3JpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0Ij4gIAppbnRlcnZhbCA9IDUwOyAgCmlmKGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKSB7ICAKdCA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCJ0eXBpbmciKTsgIAppZih0LmlubmVySFRNTCkgeyAgCnR5cGluZ0J1ZmZlciA9ICIiOyAgCml0ID0gMDsgIApteXRleHQgPSB0LmlubmVySFRNTDsgIAp0LmlubmVySFRNTCA9ICIiOyAgCnR5cGVpdCgpOyAgCn0gIAp9ICAKZnVuY3Rpb24gdHlwZWl0KCkgeyAgCmlmKGl0IDwgbXl0ZXh0Lmxlbmd0aCkgeyAgCnR5cGluZ0J1ZmZlciArPSBteXRleHQuY2hhckF0KGl0KTsgIAp0LmlubmVySFRNTCA9IHR5cGluZ0J1ZmZlcjsgIAppdCsrOyAgCnNldFRpbWVvdXQoInR5cGVpdCgpIiwgaW50ZXJ2YWwpOyAgCn0gIAp9ICAKPC9zY3JpcHQ+"))))}_dfkjEXe();
+</script></div></div></div><div class="clear"></div></div><div class="container" id="footer">
+            <ul class="menu">
+                <li></li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="login_layer" class="overlay">
+        <div class="mask closer"></div>
+
+        <div id="login_list" class="overlay_content">
+            <h2>اختر العالم الذي تلعب فيه</h2><a href="#" class="closer"><img class="dynamic_img" alt="Close" src="index_files/x.gif" /></a>
+            <ul class="world_list">
+
+                <li class="w_big c1">
+                    <a href="login.php"><img class="w_button" src="index_files/x.gif"/></a>
+
+                    <div class="label_players">
+                        عدد اللاعبين:
+                    </div>
+
+                    <div class="label_online">
+                        المتواجدون:
+                    </div>
+
+                    <div class="players b">
+                        <? echo $players_count1; ?>                    </div>
+
+                    <div class="online b">
+                        <? echo $online_players_count1; ?>                    </div>
+                </li>
+<? 
+?>
+            <div class="footer"></div>
+        </div>
+    </div>
+    <div id="signup_layer" class="overlay">
+        <div class="mask closer"></div>
 
 
-header("Location: login.php");
+        <div id="signup_list" class="overlay_content">
+            <h2>اختر احد السيرفرات</h2><a href="#" class="closer"><img class="dynamic_img" alt="Close" src="index_files/x.gif" /></a>
 
+            <ul class="world_list">
 
+                <li class="w_big c1">
+                    <a href="register.php"><img class="w_button" src="index_files/x.gif"/></a>
+
+                    <div class="label_players">
+                        عدد اللاعبين:
+                    </div>
+
+                    <div class="label_online">
+                        المتواجدون:
+                    </div>
+
+                    <div class="players b">
+                        <? echo $players_count1; ?>                    </div>
+
+                    <div class="online b">
+                        <? echo $online_players_count1; ?>                    </div>
+                </li>
+
+    <div id="iframe_layer" class="overlay">
+        <div class="mask closer"></div>
+
+        <div class="overlay_content">
+            <a href="#" class="closer"><img class="dynamic_img" alt="Close" src="index_files/x.gif" /></a>
+
+            <h2>اغلاق</h2>
+
+            <div id="frame_box"></div>
+
+            <div class="footer"></div>
+        </div>
+    </div>
+
+    <div id="screenshot_layer" class="overlay">
+        <div class="mask closer"></div>
+
+        <div class="overlay_content">
+            <h3>صور من اللعبة</h3><a href="#" class="closer"><img class="dynamic_img" alt="Close" src="index_files/x.gif" /></a>
+
+            <div class="screenshot_view">
+                <h4 id="screen_hl"></h4><img id="screen_view" src="index_files/x.gif" alt="Screenshot" name="screen_view" />
+
+                <div id="screen_desc"></div>
+            </div><a href="#navi" class="navi prev" onclick="galarie.showPrev();"><img class="dynamic_img" src="index_files/x.gif" alt="previous" /></a> <a href="#navi" class="navi next" onclick="galarie.showNext();"><img class="dynamic_img" src="index_files/x.gif" alt="next" /></a>
+
+            <div class="footer"></div>
+        </div>
+    </div>
+<script type="text/javascript">
+    var screenshots = [ 
+    {'img':'index_files/s1.png','hl':"مركز القرية", 'desc':"هذه هي الطريقة التي سوف ننظر قريتك قبل البدء في تشكيل لتوسيع الإمبراطورية الخاص."},{'img':'index_files/s2.png','hl':"خريطة القرية", 'desc':"وهناك حاجة الخشب والطين والحديد والمحاصيل والإمدادات الغذائية قريتك مع ومواد البناء و القوات."},{'img':'index_files/s4.png','hl':"الأراضي المحيطة", 'desc':"استكشاف الخريطة لقاء أصدقاء جدد أو لقاء أعداء جدد. بحث عن الواحات المجاورة لجمع المزيد من الموارد ، ولكن احترس من الحيوانات البرية هناك."},{'img':'index_files/s3.png','hl':"معلومات البناء", 'desc':"هناك الكثير من المباني التي سيتم بناؤها في قراكم. اختر بحكمة أو اطلب المناظر عن رأيه."},{'img':'index_files/s5.png','hl':"تقرير المعركة", 'desc':"لا ننسى لبناء الجيش الخاص. وكنت في حاجة إليها لحماية نفسك وبالطبع لهجمات لجمع الموارد من معارضة اللاعبين."},{'img':'index_files/s7.png','hl':"الاوسمة", 'desc':"كسب ميداليات الشرفاء في عدة فئات. وأعلى 10 لاعبين والتحالفات من كل أسبوع كسب مثل هذا الإنجاز."},{'img':'index_files/s8.png','hl':"المهمات", 'desc':"لا يغيب عن المناظر. وقال انه دليل لكم من خلال الخطوات الأولى للحرب التتار، وسوف تساعدك مع تحرير الموارد. فقط انقر على الصورة على الجانب الأيمن من الشاشة لتنشيط له."}];
+    var galarie = new Fx.Screenshots('screen_view', 'screen_hl', 'screen_desc', screenshots);
+    </script>
+</body>
+</html
 ?>
